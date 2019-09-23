a999357a51bc03f02bea0edf377c2c7545979439
etsuhisa <49114379+etsuhisa@users.noreply.github.com>
2019-09-23 14:28:27 +0900

    [new]Create
    Create New

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# jquery-table4panes
+#jQuery Table4Panes 1.0.0
+Split the table to four panes.
+
+## Demo
+Refer the following:
+* demo-jquery-table4panes.html
+
+## Usage
+
+Include jQuery and the plugin on a page.
+
+```html
+<script src="jquery.min.js"></script>
+<script src="jquery-table4panes.min.js"></script>
+```
+
+Call the table4panes function for the table to split.
+Specify the number of columns in the first parameter and the number of rows in the second parameter.
+The settings (ex. display size, display method) specify in the third parameter.
+
+```js
+$(function(){
+    $("#demo-table").table4panes(4,3,{"display-method":"flex", "width":"100%", "height":"100%", "fit":true});
+});
+```
+
+## Settings
+
+### "display-method"
+The "display-method" option can be one of the following values.
+* "inline-block"
+* "table-cell"
+* "flex"
+* "float"
+
+### "fit"
+If it is true, fit the bottom right pane fits the parent node.
+
+### Size
+The size of each pane is specified the following:
+* The overall size is specified by "height" and "width".
+* Height of each pane is specified by "top-height" and "bottom-height".
+* Width of each pane is specified by "left-width" and "right-width".
+
+### "callbacks"
+The callbacks for the events are specified by "callbacks" with event/function/data for selectors.
+
+### "css"
+The CSS maps for selectors are specified by "css".
+
+### "prefix"
+The class name prefix can be changed from the default "table4panes" by "prefix".
+
+## License
+Copyright &copy; ASAI Etsuhisa<br>
+Licensed under the MIT license.
+
---
+++ jquery-table4panes.js
@@ -0,0 +1,337 @@
+/**
+ * jquery-table4panes 1.0.0 - jQuery plugin to split the table to four panes.
+ *
+ * Copyright (c) 2019 ASAI Etsuhisa
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * [Usage]
+ * $.fn.table4panes(col_num, row_num, settings)
+ * Split the table to four panes.
+ *
+ * @param this     (in) table node to split to the panes
+ * @param col_num  (in) number of columns to fix
+ * @param row_num  (in) number of rows to fix
+ * @param settings (in) settings for table4panes
+ *   "display-method": ("inline-block"|"table-cell"|"flex"|"float") - Select the method.
+ *   "fit": (true|flase) - If true, fit the bottom right pane fits the parent node.
+ *   ("height"|"top-height"|"bottom-height"|"width"|"left-width"|"right-width"): size - Set the size.
+ *   "callbacks": {"selector1": {"event":"event1", "func":"func1", "data":"data1"},
+ *                 "selector2": {"event":"event2", "func":"func2", "data":"data2"}, ... } - Set the callbacks.
+ *   "css": {"selector1": "css-map1", "selector2": "css-map2", ... } - Map the css.
+ *   "prefix": "prefix" - Set the prefix for the class name.
+ * @return the top level div node.
+ *
+ * From:
+ * +- table id --------------------------+
+ * | (col_num + ***) x (row_num + ***)   |
+ * |                                     |
+ * | # with or without thead/tbody/th/td |
+ * +-------------------------------------+
+ *
+ * To:
+ * +- div table4panes -------------------------------------------+
+ * |      #id-table4panes                                        |
+ * |      .prefix                                                |
+ * | +- div left ----------------+ +- div right ---------------+ |
+ * | |      #id-left             | |      #id-right            | |
+ * | |      .prefix-left         | |      .prefix-right        | |
+ * | | +- div top-left --------+ | | +- div top-right -------+ | |
+ * | | |      #id-top-left     | | | |      #id-top-right    | | |
+ * | | |      .prefix-top      | | | |      .prefix-top      | | |
+ * | | | +- table -----------+ | | | | +- table -----------+ | | |
+ * | | | | col_num x row_num | | | | | | *** x row_num     | | | |
+ * | | | +-------------------+ | | | | +-------------------+ | | |
+ * | | +-----------------------+ | | +-----------------------+ | |
+ * | | +- div bottom-left -----+ | | +- div bottom-right--- -+ | |
+ * | | |      #id-bottom-left  | | | |      #id-bottom-right | | |
+ * | | |      .prefix-bottom   | | | |      .prefix-bottom   | | |
+ * | | | +- table -----------+ | | | | +- table id --------+ | | |
+ * | | | | col_num x ***     | | | | | | *** x ***         | | | |
+ * | | | +-------------------+ | | | | +-------------------+ | | |
+ * | | +-----------------------+ | | +-----------------------+ | |
+ * | +---------------------------+ +---------------------------+ |
+ * +-------------------------------------------------------------+
+ *
+ */
+
+;(function($){
+
+	/**
+	 * moveToNewTable(kind, num)
+	 * Move rows/columns to a newly created table that inherits the source table/thead/tbody.
+	 * The function treats the DOM object only, expect this.
+	 * @param this (in) source table node
+	 * @param kind (in) "row", "col"
+	 * @param num  (in) number to move
+	 * @return the created table
+	 */
+	var moveToNewTable = function(kind, num){
+		var src = this;
+		var dst = src.cloneNode(false);
+		dst.removeAttribute("id");
+		for(var i = 0; i < src.children.length && num > 0; i++){
+			var elm = src.children[i];
+			var tag = elm.nodeName.toLowerCase();
+			if(tag == "thead" || tag == "tbody" || (tag == "tr" && kind == "col")){
+				dst.appendChild(moveToNewTable.call(elm, kind, num));
+				if((tag == "thead" || tag == "tbody") && kind == "row"){
+					num -= dst.children.length;
+				}
+				if(elm.children.length <= 0){
+					elm.remove();
+					i--;
+				}
+			}
+			else if(tag == "tr" || tag == "th" || tag == "td"){
+				dst.appendChild(elm);
+				i--;
+				num--;
+			}
+		}
+		return dst;
+	}
+
+	/**
+	 * getWidthCols()
+	 * @param this (in) table node
+	 * @return the array of width of all columns in the first row.
+	 */
+	var getWidthCols = function(){
+		var arr = [];
+		$(this).find("tr:eq(0)").children().each(function(i,elm){
+			arr.push($(elm).width());
+		});
+		return arr;
+	}
+
+	/**
+	 * getHeightRows()
+	 * @param this (in) table node
+	 * @return the array of height of all rows.
+	 */
+	var getHeightRows = function(){
+		var arr = [];
+		$(this).find("tr").each(function(i,elm){
+			arr.push($(elm).height());
+		});
+		return arr;
+	}
+
+	/**
+	 * setWidthCols(arr)
+	 * Set width of all columns in the first row.
+	 * @param this (in) table node
+	 * @param arr (in) width of columns
+	 * @return this.
+	 */
+	var setWidthCols = function(arr){
+		$(this).find("tr:eq(0)").children().each(function(i,elm){
+			setFixWidth.call($(elm), arr[i]);
+		});
+		return $(this);
+	}
+
+	/**
+	 * setHeightRows(arr)
+	 * Set height of all rows.
+	 * @param this (in) table node
+	 * @param arr (in) height of rows
+	 * @return this.
+	 */
+	var setHeightRows = function(arr){
+		$(this).find("tr").each(function(i,elm){
+			setFixHeight.call($(elm), arr[i]);
+		});
+		return $(this);
+	}
+
+	/**
+	 * setFixWidth(val)
+	 * Set width/min-width/max-width of the node.
+	 * @param this (in) target node
+	 * @param val (in) width
+	 * @return this.
+	 */
+	var setFixWidth = function(val){
+		$(this).width(val);
+		$(this).css({"min-width":val, "max-width":val});
+		return $(this);
+	}
+
+	/**
+	 * setFixHeight(val)
+	 * Set height/min-height/max-height of the node.
+	 * @param this (in) target node
+	 * @param val (in) height
+	 * @return this.
+	 */
+	var setFixHeight = function(val){
+		$(this).height(val);
+		$(this).css({"min-height":val, "max-height":val});
+		return $(this);
+	}
+
+	/**
+	 * Call this function to split the table to four panes.
+	 */
+	$.fn.table4panes = function(col_num, row_num, settings){
+
+		/** Set the default class name prefix, if no prefix. */
+		var prefix = "table4panes";
+		if(settings && settings["prefix"]) prefix = settings["prefix"];
+
+		/** Decide IDs. */
+		var id_table = $(this).attr("id");
+		var id_table4panes = id_table + "-table4panes";
+		var id_left = id_table + "-left";
+		var id_right = id_table + "-right";
+		var id_top_left = id_table + "-top-left";
+		var id_bottom_left = id_table + "-bottom-left";
+		var id_top_right = id_table + "-top-right";
+		var id_bottom_right = id_table + "-bottom-right";
+
+		/** Prepare to split the table. */
+		/** Set inherited information. */
+		$(this).css({"table-layout":"fixed"});
+		$(this).addClass("pane");
+		/** Get current information before operation. */
+		var col_widths = getWidthCols.call($(this));
+		var row_heights = getHeightRows.call($(this));
+		/** Wrap table with div node. */
+		var $div_table4panes = $(this).wrap("<div id='"+id_table4panes+"'>").parent();
+
+		/** Move rows/columns. Set height/width. Wrap with div node. */
+		var $table_bottom_right = $(this);
+		var $table_bottom_left = $(moveToNewTable.call($table_bottom_right[0], "col", col_num));
+		var $div_right = $table_bottom_right.wrap("<div id='"+id_right+"'>").parent();
+		var $div_left = $table_bottom_left.wrap("<div id='"+id_left+"'>").parent();
+		setHeightRows.call($table_bottom_right, row_heights);
+		setHeightRows.call($table_bottom_left, row_heights);
+		var $table_top_right = $(moveToNewTable.call($table_bottom_right[0], "row", row_num));
+		var $table_top_left = $(moveToNewTable.call($table_bottom_left[0], "row", row_num));
+		setWidthCols.call($table_bottom_right, col_widths.slice(col_num));
+		setWidthCols.call($table_bottom_left, col_widths.slice(0,col_num));
+		setWidthCols.call($table_top_right, col_widths.slice(col_num));
+		setWidthCols.call($table_top_left, col_widths.slice(0,col_num));
+
+		/** Insert tables so that they are in the order of top left, bottom left, top right, bottom right. */
+		$table_bottom_right.before($table_top_right);
+		$table_bottom_left.before($table_top_left);
+		$div_right.before($div_left);
+
+		/** Wrap each tables with div node. */
+		var $div_bottom_right = $table_bottom_right.wrap("<div id='"+id_bottom_right+"'>").parent();
+		var $div_bottom_left = $table_bottom_left.wrap("<div id='"+id_bottom_left+"'>").parent();
+		var $div_top_right = $table_top_right.wrap("<div id='"+id_top_right+"'>").parent();
+		var $div_top_left = $table_top_left.wrap("<div id='"+id_top_left+"'>").parent();
+
+		/** Add classes for each div nodes. */
+		$div_table4panes.addClass(prefix);
+		$div_right.addClass(prefix+"-right");
+		$div_left.addClass(prefix+"-left");
+		$div_top_left.addClass(prefix+"-top");
+		$div_top_right.addClass(prefix+"-top");
+		$div_bottom_left.addClass(prefix+"-bottom");
+		$div_bottom_right.addClass(prefix+"-bottom");
+
+		/** Set overflow style for each div node. */
+		$div_bottom_right.css({"overflow-x":"scroll", "overflow-y":"scroll"});
+		$div_top_right.css({"overflow-x":"hidden", "overflow-y":"scroll"});
+		$div_bottom_left.css({"overflow-x":"scroll", "overflow-y":"hidden"});
+		$div_top_left.css({"overflow-x":"hidden", "overflow-y":"hidden"});
+
+		/** Set the side-by-side CSS property. */
+		switch(settings["display-method"]){
+		case "inline-block" :
+			$div_left.css({"display":"inline-block"});
+			$div_right.css({"display":"inline-block"});
+			break;
+		case "table-cell" :
+			$div_table4panes.css({"display":"table", "table-layout":"fixed", "margin":"0 auto"});
+			$div_left.css({"display":"table-cell"});
+			$div_right.css({"display":"table-cell"});
+			break;
+		case "flex" :
+			$div_table4panes.css({"display":"flex"});
+			break;
+		case "float" :
+		default :
+			$div_left.css({"float":"left"});
+			break;
+		}
+
+		/** Set height/width from settings. */
+		$.each(settings, function(param, value){
+			switch(param){
+			case "height" :
+				setFixHeight.call($div_table4panes, settings[param]);
+				break;
+			case "top-height" :
+				setFixHeight.call($div_top_left, settings[param]);
+				setFixHeight.call($div_top_right, settings[param]);
+				break;
+			case "bottom-height" :
+				setFixHeight.call($div_bottom_left, settings[param]);
+				setFixHeight.call($div_bottom_right, settings[param]);
+				break;
+			case "width" :
+				setFixWidth.call($div_table4panes, settings[param]);
+				break;
+			case "left-width" :
+				setFixWidth.call($div_top_left, settings[param]);
+				setFixWidth.call($div_bottom_left, settings[param]);
+				break;
+			case "right-width" :
+				setFixWidth.call($div_top_right, settings[param]);
+				setFixWidth.call($div_bottom_right, settings[param]);
+				break;
+			}
+		});
+
+		/** Fit the parent div container. */
+		/** Recalculate height/width of bottom right when resizing. */
+		if(settings["fit"]){
+			var dw = $div_right.outerWidth(true)-$div_top_right.width();
+			var dh = $div_bottom_left.outerHeight(true)-$div_bottom_left.height();
+			$(window).resize(function(){
+				var w = $div_table4panes.width()-$div_left.outerWidth(true)-dw;
+				var h = $div_table4panes.height()-$div_top_left.outerHeight(true)-dh;
+				setFixWidth.call($div_top_right, w);
+				setFixWidth.call($div_bottom_right, w);
+				setFixHeight.call($div_bottom_left, h);
+				setFixHeight.call($div_bottom_right, h);
+			});
+			$(window).trigger('resize');
+		}
+
+		/** Set CSS properties from settings. */
+		if(settings["css"]){
+			$.each(settings["css"], function(sel, map){
+				$(sel).css(map);
+			});
+		}
+
+		/** Set event actions from settings. */
+		if(settings["callbacks"]){
+			$.each(settings["callbacks"], function(sel, action){
+				$(sel).on(action.event, action.data, action.func);
+			});
+		}
+
+		/** Set scroll events to move in sync. */
+		$div_top_right.scroll(function(){
+			$div_top_left.scrollTop($(this).scrollTop());
+		});
+		$div_bottom_left.scroll(function(){
+			$div_top_left.scrollLeft($(this).scrollLeft());
+		});
+		$div_bottom_right.scroll(function(){
+			$div_bottom_left.scrollTop($(this).scrollTop());
+			$div_top_right.scrollLeft($(this).scrollLeft());
+		});
+
+		/** Return the top level node. */
+		return $div_table4panes;
+	}
+})(jQuery);
